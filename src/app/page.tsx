5e82ffdc1bb6ba3ddfe80b2d3fc3a4fc359cafd5
xscriptorcode <preciado.oscar.osorio@gmail.com>
2025-07-01 18:04:12 +0200

    dimentions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ import ThemeToggle from "./components/icons/ThemeToggle";
 export default function Home() {
   return (
       <main className="">
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center ">
         <ThemeToggle />
         </div>
-      <h1>Lines:</h1>
-      <div className="flex flex-wrap gap-4">
+      <h1 className="flex items-cenyer justify-center pt-8">Lines:</h1>
+      <div className="flex flex-wrap gap-4 items-center justify-center pt-8">
         <FacebookIconGlass / >
         <InstagramIconGlass />
         <ComponentIconGlass />
