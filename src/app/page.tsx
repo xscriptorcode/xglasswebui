f2948c8704403c9b31c9212deaac9848ee80c86d
xscriptorcode <preciado.oscar.osorio@gmail.com>
2025-07-01 14:11:59 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      <FacebookIconGlass / >
+      <div className="flex flex-col">
+        <h1>Lines:</h1>
+        <FacebookIconGlass / >
+      </div>
       </main>
     </div>
   );
